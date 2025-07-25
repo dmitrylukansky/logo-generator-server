bb6ccc4fec056632d19ee691982efefaad269cc4
Dzmitry <127845549+dmitrylukansky@users.noreply.github.com>
2025-07-26 00:14:37 +0300

    Update script.js
    add adress

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ document.getElementById("aiNameBtn").addEventListener("click", async () => {
   if (!keyword) return alert("Введите ключевое слово");
 
   try {
-    const response = await fetch("http://localhost:3000/generate-name", {
+    const response = await fetch("https://logo-generator-api.onrender.com/generate-name", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
